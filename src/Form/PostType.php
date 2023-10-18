70357dc3566ee0ef0d456bf6aeb114ee949f8383
Dekpo Yologaza <dekpo@me.com>
2023-10-18 12:05:31 +0200

    Made Post CRUD and template form...

---
+++ src/Form/PostType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('category',EntityType::class,[
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'attr' => ['class' => 'form-select']
+            ])
+            ->add('title')
+            ->add('description')
+            ->add('image')
+            ->add('content')
+            ->add('pinned')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('tags',EntityType::class,[
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => ['class' => 'form-check']
+            ])
+            // ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
