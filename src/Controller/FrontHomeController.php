41ef968719ce811f7538f43a9132ec1b9814a8e3
Dekpo Yologaza <dekpo@me.com>
2023-10-19 03:31:00 +0200

    Home front posts card and stuff

--- src/Controller/FrontHomeController.php
+++ src/Controller/FrontHomeController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PostRepository;
 
-class HomeController extends AbstractController
+class FrontHomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+                'posts' => $postRepository->findBy(array(), array('updatedAt' => 'DESC')),
         ]);
     }
 }
